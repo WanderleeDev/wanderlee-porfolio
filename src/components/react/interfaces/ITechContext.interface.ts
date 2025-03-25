f400731e94b948228eca980a9fe04a03fdad5009
Wanderlee <wanderleedev@gmail.com>
2025-03-24 23:28:25 -0500

    refactor: :fire: Unused files

--- src/components/react/interfaces/ITechContext.interface.ts
+++
@@ -1,6 +0,0 @@
-import { type ITechGalleryData } from "../../../interfaces/ITechnology.interface" 
-
-export interface ITechContext {
-  gallery: ITechGalleryData
-  setTechIcons: (data: ITechGalleryData) => void
-}
